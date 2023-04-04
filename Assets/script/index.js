b06fb8f5604e2c44645adeba62c6253cef1c1eed
Rushit <rushitparmar59@gmail.com>
2023-04-04 11:16:55 -0500

    scoreboard created

--- Assets/script/index.js
+++ Assets/script/index.js
@@ -7,11 +7,15 @@ const input = document.querySelector('.input');
 const startButton = document.querySelector('.startButton');
 const playAgain = document.querySelector('.playAgain');
 const score = document.querySelector('.score');
+const points = document.querySelector('.hits');
+const scoreboard = document.querySelector('.scoreboard');
+
 
 // Creating new Audio Object 
 const bgMusic = new Audio('./Assets/Audio/tick-tock-21075.mp3');
 bgMusic.type = 'Audio/mp3';
 bgMusic.volume = 0.4;
+bgMusic.loop = true;
 
 const winWord = new Audio('./Assets/Audio/winpoint.mp3');
 winWord.type = 'Audio/mp3';
@@ -80,6 +84,7 @@ function randomWord() {
 function checkWord() {
   if(displayWord.innerText === input.value) {
     hits++;
+    points.innerText = `Hits: ${hits}`;
     winWord.play();
     percentage = ((hits / 90) * 100).toFixed(2);
     displayWord.innerText = '';
@@ -94,8 +99,32 @@ function time() {
   count.innerHTML = countdown;
 }
 
+
+// scoreboard
+const scoreArray = [];
+
+// function for creating score divs
+function scoreBoardDiv(toString) {
+  scoreboard.innerHTML = '<h3>High Scores</h3>';
+  const toObject = JSON.parse(toString);
+
+  toObject.forEach(element => {
+    scoreboard.innerHTML +=
+    `<div class="score-flex">
+      <div>#${toObject.indexOf(element) + 1}</div>
+      <div>${element.hits}</div>
+      <div>${element.percentage}%</div>
+     </div>
+    `
+  });
+}
+
+
+
+
 // start button
 startButton.addEventListener('click', function() {
+  input.focus();
   count.innerHTML = countdown;
   countTime = setInterval(function() { time(); }, 1000);
   bgMusic.play();
@@ -103,26 +132,59 @@ startButton.addEventListener('click', function() {
   wordCheck = setInterval(function() { checkWord(); }, 100);
   startButton.style.display = 'none';
   playAgain.style.display = 'block';
+
+  // checking when to stop the game
+  const stop = setInterval(function() {
+    if(countdown === 0) {
+      gameOver.play();
+      bgMusic.pause();
+      clearInterval(stop);
+      clearInterval(countTime);
+      displayWord.innerHTML = '';
+      input.value = '';
+      count.innerHTML = '0';
+      clearInterval(wordCheck);
+      score.style.display = 'block';
+      playerScore = new Score(getDate(), hits, percentage);
+      score.innerHTML = `Date: ${playerScore.date}<br>Hits: ${playerScore.hits}<br>Percentage: ${playerScore.percentage}%`;
+
+      // new score
+      const newScore = {
+        hits: playerScore.hits,
+        percentage: playerScore.percentage
+      }
+      // pushing the new score
+      scoreArray.push(newScore);
+      scoreArray.sort((a, b) => b.hits - a.hits);
+      scoreArray.splice(9);
+
+      // scoreArray to string
+      const toString = JSON.stringify(scoreArray);
+      localStorage.setItem('scoreboard', toString);
+
+      scoreBoardDiv(toString);
+      scoreboard.classList.add('active');
+    }  
+  }, 1000);
+
 });
 
-// checking when to stop the game
-stop = setInterval(function() {
-  if(countdown === 0) {
-    gameOver.play();
-    bgMusic.pause();
-    clearInterval(stop);
-    clearInterval(countTime);
-    displayWord.innerHTML = '';
-    input.value = '';
-    count.innerHTML = '0';
-    clearInterval(wordCheck);
-    score.style.display = 'block';
-    playerScore = new Score(getDate(), hits, percentage);
-    score.innerHTML = `Date: ${playerScore.date}<br>Hits: ${playerScore.hits}<br>Percentage: ${playerScore.percentage}%`
-  }  
-}, 1000);
 
 // Play again button 
 playAgain.addEventListener('click', function() {
-  location.reload();
+  clearInterval(stop);
+  clearInterval(countTime);
+  displayWord.innerHTML = '';
+  input.value = '';
+  count.innerHTML = `- -`;
+  countdown = 99;
+  hits = 0;
+  clearInterval(wordCheck);
+  bgMusic.currentTime = 0;
+  score.style.display = 'none';
+  points.innerHTML = `Hits: 0`;
+  playAgain.style.display = 'none';
+  startButton.style.display = 'inline-block';
+  scoreboard.classList.remove('active');
+  copy = [...words];
 });
